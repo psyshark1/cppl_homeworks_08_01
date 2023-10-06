e27b172f14c6c554dd7a2ada4eb1e49aa61926d1
Andrey <andrey.zalozhenkov@pikta.ru>
2023-10-06 13:27:56 +0400

    Добавьте файлы проекта.

---
+++ 01.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+std::ostream& operator << (std::ostream& l, std::vector<int>& r)
+{
+	for (const auto& it : r)
+	{
+		l << it << ' ';
+	}
+	return l;
+}
+
+int main()
+{
+	std::vector<int> vec{ 1, 1, 2, 5, 6, 1, 2, 4 };
+	std::sort(vec.begin(), vec.end(), [](const int& lhs, const int& rhs) {return lhs < rhs; });
+	auto nend = std::unique(vec.begin(), vec.end());
+	vec.erase(nend, vec.end());
+	std::cout << vec;
+	return 0;
+}
